dc24c2c18601986421911bbcff42d42292a963a0
littleblack111 <jerrylam110209@gmail.com>
2023-03-19 08:38:14 +0800

    update

--- reflector.sh
+++ reflector.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/bash
 while true; do
-    exec sudo /usr/bin/reflector @/etc/xdg/reflector/reflector.conf &
-    update
+    #sudo #exec sudo /usr/bin/reflector @/etc/xdg/reflector/reflector.conf &
+    sudo npm update -g & pipx upgrade-all & yay -Syu --noconfirm --disable-download-timeout && flatpak update -y
     sleep 1080
 done
--- runbgblur.sh
+++ runbgblur.sh
@@ -2,7 +2,7 @@
 
 while true; do
     pkill -f backgroundblur
-    wallpaper="$(find /home/system/Pictures/wallpapers -type f -name '*.jpg' -o -name '*.png' | shuf -n 1)"
+    wallpaper="$(find /home/system/Pictures/wallpapers -type f -name '*.jpg' -o -name '*.png' | shuf -n 1)"    
     feh --bg-fill "$wallpaper" &
     $HOME/.config/bspwm/scripts/backgroundblur -i "$wallpaper" &
     sleep 1800
