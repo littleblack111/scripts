d3962b826c02b66265bd2617d231f9c22ffc6445
littleblack111 <jerrylam110209@gmail.com>
2023-05-17 17:06:35 +0800

    update

--- auto-update.sh
+++ auto-update.sh
@@ -5,7 +5,8 @@ while true; do
     if [[ "$(systemctl is-system-running)" == "running" ]]; then
         if [[ "$(xprintidle)" -gt 600000 ]]; then  # Changed from 600000 (10 minutes) to 3600000 (1 hour)
             #sudo #exec sudo /usr/bin/reflector @/etc/xdg/reflector/reflector.conf &
-            notify-send "Auto-Updater" "Update in progress, please don't poweroff the computer" && sudo npm update -g & nix-env -u & yay -Syu --noconfirm --disable-download-timeout && flatpak update -y && notify-send -u low "Auto-Updater" "Update Successfully!" && echo \#sleep 3600 || notify-send -u critical "Auto-Updater" "Update Faild!" # pipx upgrade-all
+            touch /home/system/updated-via-auto-update
+            notify-send "Auto-Updater" "Update in progress, please don't poweroff the computer" && sudo npm update -g & nix-env -u & brew update & yay -Sl > /tmp/yaySl & yay -Syu --noconfirm --disable-download-timeout && flatpak update -y && notify-send -u low "Auto-Updater" "Update Successfully!" && echo \#sleep 3600 || notify-send -u critical "Auto-Updater" "Update Faild!" # pipx upgrade-all
         else
             sleep 60
         fi
