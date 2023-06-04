635c736432170b42cd8325f52481508fb557c0ac
littleblack111 <jerrylam110209@gmail.com>
2023-06-04 13:45:31 +0800

    Fixes and finalized auto-update, idle, etc's format

---
+++ autoclicker-left.py
@@ -0,0 +1,19 @@
+from time import sleep
+from random import uniform
+from pynput.mouse import Controller, Button
+from sys import argv
+
+mouse = Controller()
+
+try:
+    if len(argv) > 1:
+        delay = int(argv[1]) // 1000
+        while True:
+            mouse.click(Button.left, 1)
+            sleep(delay)
+    else:
+        while True:
+            mouse.click(Button.left, 1)
+            sleep(round(uniform(0.035, 0.055), 3))
+except KeyboardInterrupt:
+    print("\nKeyboardInterrupt, exiting")
--- idle.sh
+++ idle.sh
@@ -4,8 +4,16 @@
 while true; do
     if [[ "$(systemctl is-system-running)" == "running" || "$(systemctl is-system-running)" == "degraded" ]]; then
         if [[ "$(xprintidle)" -gt 600000 ]]; then  # Changed from 600000 (10 minutes) to 3600000 (1 hour)
-            $HOME/scripts/auto-update.sh &
-            killall picom; bspcomp &
+            if [ ! -f /var/lock/idle.lock ]; then
+                $HOME/scripts/update.sh &
+                killall picom; bspcomp &
+                sudo touch /var/lock/idle.lock
+            elif [[ "$(xprintidle)" -le 50 ]]; then
+                sudo rm -f /var/lock/idle.lock
+                continue
+            else
+                sleep 5
+            fi
         else
             sleep 60
         fi
