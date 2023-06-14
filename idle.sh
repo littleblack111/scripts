bd7784d182c0c7609d419817b18462283e51062d
littleblack111 <jerrylam110209@gmail.com>
2023-06-14 20:40:56 +0800

    update

--- idle.sh
+++ idle.sh
@@ -5,12 +5,12 @@ while true; do
     if [[ "$(systemctl is-system-running)" == "running" || "$(systemctl is-system-running)" == "degraded" ]]; then
         if [[ "$(xprintidle)" -gt 600000 ]]; then  # Changed from 600000 (10 minutes) to 3600000 (1 hour)
             if [ ! -f /var/lock/idle.lock ]; then
+                sudo touch /var/lock/idle.lock
                 $HOME/scripts/update.sh &
                 killall picom; bspcomp &
                 sleep 350
                 killall update.sh
-                sudo touch /var/lock/idle.lock
-            elif [[ "$(xprintidle)" -le 50 ]]; then
+            elif [[ "$(xprintidle)" -le 100 ]]; then
                 sudo rm -f /var/lock/idle.lock
                 continue
             else
