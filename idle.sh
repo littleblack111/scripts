8c77a6981602d68e953f1395ce63e0ddb7cfd953
littleblack111 <jerrylam110209@gmail.com>
2023-06-25 21:57:04 +0800

    improved scripts syntax and made them more logical

--- idle.sh
+++ idle.sh
@@ -3,26 +3,21 @@
 
 while true; do
     if [[ "$(systemctl is-system-running)" == "running" || "$(systemctl is-system-running)" == "degraded" ]]; then
-        if [[ "$(xprintidle)" -gt 600000 ]]; then  # Changed from 600000 (10 minutes) to 3600000 (1 hour)
-            if [ ! -f /var/lock/idle.lock ]; then
-                sudo touch /var/lock/idle.lock
-                $HOME/scripts/update.sh &
-                NekoMC --pause &
-                killall picom; bspcomp &
-                sleep 350
-                killall update.sh
-                freezeapp &
-                continue
-            elif [[ "$(xprintidle)" -le 100 ]]; then
-                sudo rm -f /var/lock/idle.lock
-                unfreezeapp &
-                NekoMC --play &
-                continue
-            else
-                sleep 5
-            fi
+        if [[ "$(xprintidle)" -gt 600000 && ! -f /var/lock/idle.lock ]]; then  # Changed from 600000 (10 minutes) to 3600000 (1 hour)
+            sudo touch /var/lock/idle.lock
+            $HOME/scripts/update.sh &
+            NekoMC --pause &
+            killall picom; bspcomp &
+            freezeapp &
+            continue
+        elif [[ "$(xprintidle)" -le 100 && -f /var/lock/idle.lock ]]; then
+            sudo rm -f /var/lock/idle.lock
+            killall update.sh yay
+            unfreezeapp &
+            #NekoMC --play &
+            continue
         else
-            sleep 60
+            sleep 1
         fi
     fi
 done
