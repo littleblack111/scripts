80fe88409c8e8837d90a92f2cbcff94d345fb632
littleblack111 <jerrylam110209@gmail.com>
2023-07-23 14:38:01 +0800

    updated

--- idle.sh
+++ idle.sh
@@ -2,13 +2,15 @@
 
 mutev=true # stop volume/sound(mute) - comment or un-comment
 pmusic=true # stop/start music from playing - command or un-comment
+IDLETIME=1800000
+IDLEBACKTIME=100
 
 
 while true; do
     if [[ "$(systemctl is-system-running)" == "running" || "$(systemctl is-system-running)" == "degraded" ]]; then
-        if [[ "$(xprintidle)" -gt 600000 && ! -f /var/lock/idle.lock ]]; then  # Changed from 600000 (10 minutes) to 3600000 (1 hour)
-            printf "User is afk in %s\n", "$(date)"
-            sudo touch /var/lock/idle.lock
+        if [[ "$(xprintidle)" -gt $IDLETIME && ! -f /tmp/lock/idle.lock ]]; then  # Changed from 600000 (10 minutes) to 3600000 (1 hour)
+            printf "User is idle(afk) in %s\n", "$(date)"
+            sudo /sbin/touch /tmp/lock/idle.lock
             #if [ $mutev ]; then
             #    soundlevel=$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))
             #    amixer set Master 0%
@@ -27,17 +29,21 @@ while true; do
             if [ $pmusic ] && [ $m ]; then
                 NekoMC --pause &
             fi
-            # killall picom; bspcomp & # paused in freezeapp and unfreezeapp
+            #killall picom; bspcomp & # paused in freezeapp and unfreezeapp
+            # Disable mouse just in case accidental awake for high DPI rate mouse
+            xinput --disable 12
             freezeapp &
             continue
-        elif [[ "$(xprintidle)" -le 100 && -f /var/lock/idle.lock ]]; then
-            printf "User is back from afk in %s\n", "$(date)"
+        elif [[ "$(xprintidle)" -le $IDLEBACKTIME && -f /tmp/lock/idle.lock ]]; then
+            printf "User is back from idle(afk) in %s\n", "$(date)"
             #if [ $mutev ] && [ $soundlevel ]; then
             #    amixer set Master $soundlevel
             #    unset $soundlevel
             #elif [ $mutev ] && [ ! $soundlevel ]; then
             #    notify-send "ERROR: \$soundlevel is not set, unknown sound level..."
             #fi
+            # Re-enable mouse
+            xinput --enable 12
             if [ $mutev ] && [[ $(pactl get-sink-mute @DEFAULT_SINK@) == "Mute: yes" ]]; then
                 amixer set Master unmute
             elif [ $mutev ] && [[ $(pactl get-sink-mute @DEFAULT_SINK@) == "Mute: no" ]]; then
@@ -51,7 +57,7 @@ while true; do
                 NekoMC --play &
                 unset m
             fi
-            sudo rm -f /var/lock/idle.lock
+            sudo /sbin/rm -f /tmp/lock/idle.lock
             continue
         else
             sleep 1
--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# check if db.lck exist(other instant exist) and deal with it
 if [ -f /var/lib/pacman/db.lck ]; then
     notify-send "db.lck exist, waiting 1 min until rm -f"
     sleep 60
@@ -9,5 +10,5 @@ if [ -f /var/lib/pacman/db.lck ]; then
     fi
 fi
 
-sudo npm update -g & nix-env -u & brew update & blck=$! & yay -Sl > /tmp/yaySl & yay -Ss > /tmp/yaySs & yay --noconfirm --disable-download-timeout & wait "$blck" && brew upgrade || sudo touch /var/lock/updatefail.lock # notify-send -u critical "Auto-Updater" "Update Faild!" # pipx upgrade-all
+sudo npm update -g & nix-env -u & brew update & blck=$! & yay -Sl > /tmp/yaySl & yay -Ss > /tmp/yaySs & yay --noconfirm --disable-download-timeout & wait "$blck" && brew upgrade || sudo /sbin/touch /tmp/lock/updatefail.lock # notify-send -u critical "Auto-Updater" "Update Faild!" # pipx upgrade-all
 #notify-send "Auto-Updater" "Update in progress, please don't poweroff the computer" && sudo npm update -g & nix-env -u & brew update & brew upgrade & yay -Sl > /tmp/yaySl & yay -Ss > /tmp/yaySs & yay -Syu --noconfirm --disable-download-timeout && notify-send -u low "Auto-Updater" "Update Successfully!" || notify-send -u critical "Auto-Updater" "Update Faild!" # pipx upgrade-all
