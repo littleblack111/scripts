ff46714d709131d73cc16e0d7fdba7a6990d9eda
littleblack111 <littleblack11111@gmail.com>
2024-07-12 10:57:23 +0800

    added more pm

--- update.sh
+++ update.sh
@@ -10,5 +10,5 @@ if [ -f /var/lib/pacman/db.lck ]; then
     fi
 fi
 
-tldr --update & sudo npm update -g & nix-env -u & /usr/local/linuxbrew/bin/brew update & blck=$! & yay -Sl > /tmp/yaySl & yay -Ss > /tmp/yaySs & yay --noconfirm --disable-download-timeout & wait $blck && /opt/homebrew/bin/brew upgrade || sudo /sbin/touch /tmp/lock/updatefail.lock # notify-send -u critical "Auto-Updater" "Update Faild!" # pipx upgrade-all
+tldr --update & sudo npm update -g & nix-env -u & /usr/local/linuxbrew/bin/brew update & blck=$! & yay -Sl > /tmp/yaySl & yay -Ss > /tmp/yaySs & yay --noconfirm --disable-download-timeout & wait $blck && /opt/homebrew/bin/brew upgrade & flatpak update -y  & tldr -u|| sudo /sbin/touch /tmp/lock/updatefail.lock # notify-send -u critical "Auto-Updater" "Update Faild!" # pipx upgrade-all
 #notify-send "Auto-Updater" "Update in progress, please don't poweroff the computer" && sudo npm update -g & nix-env -u & /usr/local/linuxbrew/bin/brew update & /usr/local/linuxbrew/bin/brew upgrade & yay -Sl > /tmp/yaySl & yay -Ss > /tmp/yaySs & yay -Syu --noconfirm --disable-download-timeout && notify-send -u low "Auto-Updater" "Update Successfully!" || notify-send -u critical "Auto-Updater" "Update Faild!" # pipx upgrade-all
