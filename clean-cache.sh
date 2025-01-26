b2a3545d7445e9029fc12671ee5ae2762aa66515
littleblack111 <littleblack11111@gmail.com>
2025-01-27 00:03:17 +0800

    update(clean-cache): more stuff to clean
    feat: get sudo perm first

--- clean-cache.sh
+++ clean-cache.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# get perm
+sudo -v
+
 # YARN
 yarn cache clean || sudo yarn cache clean
 
@@ -15,3 +18,13 @@ sudo pacman -Scc --noconfirm
 # trash
 trash-empty 30 -f
 sudo trash-empty 30 -f
+sudo -E trash-empty 30 -f
+
+# AUR archbuilds
+sudo rm -rf /var/lib/archbuild
+
+# debtap
+sudo rm -rf /var/cache/debtap
+
+# vscode cpptools
+rm -rf $XDG_CACHE_HOME/vscode-cpptools
