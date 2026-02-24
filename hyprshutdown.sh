efa790b4dcd0cffada76566316153f4b2d36bafe
littleblack111 <littleblack11111@gmail.com>
2026-02-25 01:21:07 +0800

    hyprshutdown: consider dupe

--- hyprshutdown.sh
+++ hyprshutdown.sh
@@ -1,14 +1,30 @@
-#!/usr/bin/env sh
+#!/usr/bin/bash
 
 WAIT=10 # in seconds
 self=$$
 cancel="Cancel"
 confirm="Confirm"
+DUP_ACTION=kill # or shutdown
+DUP_AMOUNT=3 # this only matters if its shutdown
+
+other_pids=$(pidof -x -o $$ "$(basename "$0")")
+dup_count=$(echo "$other_pids" | wc -w)
+
+if [[ $DUP_ACTION == "kill" ]]; then
+	if [ $dup_count -gt 0 ]; then
+		kill $other_pids
+	fi
+elif [[ $DUP_ACTION == "shutdown" ]]; then
+	if [ $dup_count -ge DUP_AMOUNT ]; then
+		hyprshutdown &!
+		kill $other_pids
+	fi
+fi
 
 function dialog {
 	ans=$(timeout 1.2 hyprland-dialog --apptitle hyprshutdown --title "Shutting down in $1" --buttons "$cancel;$confirm")
 	if [[ $ans == $confirm ]]; then
-		hyprshutdown
+		hyprshutdown &!
 		kill $self
 	elif [[ $ans == $cancel ]]; then
 		kill $self
